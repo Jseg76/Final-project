de6428a1c29c628585c02a28577bbeccef8276ec
Jseg76 <650joshua@gmail.com>
2026-06-01 13:28:54 -0700

    Merge branch 'main' of https://github.com/Jseg76/Final-project
    # Conflicts:
    #	classes.py
    #	client.py
    j

--- client.py
+++ client.py
@@ -5,19 +5,16 @@ pygame.init()
 running = True; clock = pygame.time.Clock()
 win = pygame.display.set_mode((800, 700))
 
-player = Player()
-
 tiles = []
 
 for j in range(20):
     row = []
     for i in range(12):
-        row.append(BackgroundTile(tileSize * i + 250, tileSize * j + 70,'backgroundtile.png', (tileSize,tileSize)))
+        row.append(BackgroundTile(tileSize * i + 250, tileSize * j + 70,'bgTile.png', (tileSize,tileSize)))
     tiles.append(row)
 
-test = Text(300, 20, 'This is text', 15, None, (255, 0, 0))
 
-blocks = []
+test = Text(300, 20, 'This is text', 15, None, (255, 0, 0))
 
 while running:
     for event in pygame.event.get():
